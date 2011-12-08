spree/spree

commit cca8e6b5d945cf6c7edfbd501b3ef4a763de3600

Prompt for creation of a new admin user if one already exists when running rake db:admin:create

auth/db/default/users.rb
@@ -51,5 +51,16 @@ def create_admin_user
 end
 
 if Rails.env.development?
-  create_admin_user if Spree::User.where("#{Spree::Role.table_name}.name" => "admin").includes(:roles).empty?
+  if User.where("roles.name" => 'admin').includes(:roles).empty?
+    create_admin_user
+  else
+    puts "Admin user has already been previsouly created."
+    puts "Would you like to create a new admin user?"
+    res = STDIN.gets.chomp
+    if res =~ /y[es]*/
+      create_admin_user
+    else
+      puts "No admin user created."
+    end
+  end
 end

badogar/solidus

commit 2165a6222807a02d176d0c8d6a032774f6a8e2e2

Fix typo introduced in cca8e6b5d945cf6c7edfbd501b3ef4a763de3600

auth/db/default/users.rb
@@ -55,7 +55,7 @@ if Rails.env.development?
   if Spree::User.admin.empty?
     create_admin_user
   else
-    puts 'Admin user has already been previsouly created.'
+    puts 'Admin user has already been previously created.'
     if agree('Would you like to create a new admin user? (yes/no)')
       create_admin_user
     else