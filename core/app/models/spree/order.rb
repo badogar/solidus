paulgonz6/spree

Fix issue with multiple use of same code.
If multiple orders make it to the confirm step with the same promotion
code, it would correctly make the adjustment ineligible but wouldn't
update the totals.

core/app/models/spree/order.rb
@@ -559,6 +559,13 @@ module Spree
         end
       end
 
+      def ensure_promotions_eligible
+        updater.update_promo_total
+        if promo_total_changed?
+          errors.add(:base, Spree.t(:promotion_total_changed_before_complete)) and return false
+        end
+      end
+
       def validate_line_item_availability
         availability_validator = Spree::Stock::AvailabilityValidator.new
 

badogar/solidus

Conflicts:
	core/app/models/spree/order.rb
	core/app/models/spree/order/checkout.rb
	core/spec/models/spree/order/checkout_spec.rb

core/app/models/spree/order.rb
@@ -721,6 +721,13 @@ module Spree
       end
     end
 
+    def ensure_promotions_eligible
+      updater.update_adjustment_total
+      if promo_total_changed?
+        errors.add(:base, Spree.t(:promotion_total_changed_before_complete)) and return false
+      end
+    end
+
     def validate_line_item_availability
       availability_validator = Spree::Stock::AvailabilityValidator.new
 